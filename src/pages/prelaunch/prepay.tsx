7b70c5070816f8eec99ab608ea793525a8208a50
Njeririziki <rizikinjeri@gmail.com>
2023-05-04 13:18:08 +0300

    final fonts

---
+++ src/pages/prelaunch/prepay.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/layouts/default";
+
+const PrelaunchPay = () => {
+    return ( 
+        <MainLayout>
+    
+        </MainLayout>
+     );
+}
+ 
+export default PrelaunchPay;
