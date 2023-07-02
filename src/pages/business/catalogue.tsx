1ba066b4713ff9894b4635e9efea0d559671ce06
njeririziki <rizikinjeri@gmail.com>
2023-07-02 20:28:03 +0300

    businessmodule

---
+++ src/pages/business/catalogue.tsx
@@ -0,0 +1,19 @@
+import HeaderSiderLayout from "@/components/layouts/HeaderSiderLayout";
+import SearchableTable from "@/components/reusable/Table";
+
+const CataloguePage = () => {
+    return ( 
+        <HeaderSiderLayout>
+             <div  className="w-5/6  pt-8 h-full ">
+             <h2 className="font-semibold text-lg text-black" > Products </h2>
+             <div className='pt-8'>
+             <SearchableTable/>
+             </div>
+              
+               
+             </div>
+            
+        </HeaderSiderLayout>
+     );
+} 
+export default CataloguePage;
