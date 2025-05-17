25730dd316c255bc633352cb253b02bb5bc79665
njeririziki <rizikinjeri@gmail.com>
2025-05-17 13:48:23 +0300

    cleanup

--- src/hooks/useBusiness.ts
+++ src/hooks/useBusiness.ts
@@ -1,29 +1,26 @@
-import axiosInstance from "@/utils/AxiosInstance";
+import { supabase } from "@/utils/supabaseClient";
 
 const useBusiness = () => {
-    const addBusiness = (values: any) => {
-        new Promise<void>(async(resolve, reject) => {
-         try {
-            await axiosInstance.post(`/business`, values)
-             .then(res=>{
-              console.log(res)
-                 if(res.data.success){
-                    console.log(res.data);
-                   resolve(res.data)
-                }
-                reject(res.data)
-               
-              }).catch(err=>console.log(err))
-            
-            } catch (error) {
-              alert('add business failed' + error);
-            }
-        })
-      
-       };
-    return {
-       addBusiness
-    };
-}
- 
+  const addBusiness = (values: any) => {
+    new Promise<void>(async (resolve, reject) => {
+      try {
+        const { data, error } = await supabase
+          .from("Business")
+          .insert([values]);
+
+        if (data) {
+          resolve(data);
+        }
+
+        if (error) throw error;
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+  return {
+    addBusiness,
+  };
+};
+
 export default useBusiness;
